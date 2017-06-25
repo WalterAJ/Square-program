0b9193ba3c88ef954d65d9e3d2438e70ad0342b0
2015210405004 <a960134938@qq.com>
2017-06-25 20:40:16 +0800

    6 25

--- js/booking-stepo.js
+++ js/booking-stepo.js
@@ -7,3 +7,15 @@
 $(function() {
  $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
 });
+ $(".nav-button").on("click",function(){
+
+        var aps = $(".nav-panel");
+       aps.animate({"right":" 0 ","opacity":"1"},300)
+      
+
+    });
+    $(".menu-close").on("click",function(){
+        var aps = $(".nav-panel");
+       aps.animate({"right":"-1000","opacity":"1"},300)
+      
+    });
--- js/booking-steps.js
+++ js/booking-steps.js
@@ -10,3 +10,15 @@
      
      
     });
+ $(".nav-button").on("click",function(){
+
+        var aps = $(".nav-panel");
+       aps.animate({"right":" 0 ","opacity":"1"},300)
+      
+
+    });
+    $(".menu-close").on("click",function(){
+        var aps = $(".nav-panel");
+       aps.animate({"right":"-1000","opacity":"1"},300)
+      
+    });
