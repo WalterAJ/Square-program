97995ea0c3109d249bf01c0a70336255b6c6ab97
2015210405004 <a960134938@qq.com>
2017-06-27 10:05:18 +0800

    nav-final

---
+++ js/booking-stepf.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-06-22 20:14:06
+ * @version $Id$
+ */
+
+$(".nav-button").on("click", function() {
+
+	var aps = $(".nav-panel");
+	aps.animate({
+		"right": " 0 ",
+		"opacity": "1"
+	}, 300)
+
+
+});
+$(".menu-close").on("click", function() {
+	var aps = $(".nav-panel");
+	aps.animate({
+		"right": "-1000",
+		"opacity": "1"
+	}, 300)
+
+});
