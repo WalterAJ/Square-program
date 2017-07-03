a250466daafdb1943c2f09cd308a64b8330c449e
2015210405004 <a960134938@qq.com>
2017-07-03 11:23:17 +0800

    Json

--- js/booking-steps.js
+++ js/booking-steps.js
@@ -1,27 +1,42 @@
-
 $(initPage);
+
 function initPage() {
-$.getJSON("Json/Therapist.json", function(data) {
-	headData = data.body;
-	var headHtml = $.templates("#massagistTmpl").render(headData);
-	console.log(headHtml);
-	$(".booking-therapist").append(headHtml);
+  $.getJSON("Json/Therapist.json", function(data) {
+    headData = data.body;
+    var headHtml = $.templates("#massagistTmpl").render(headData);
+    
+    $(".booking-therapist").append(headHtml);
+  });
+   $(".booking-title").on("click", function() {
+  console.log(1);
+
 });
+
 }
- $(".dropdown>ul li").on("click", function(){
-     $(this).parent().siblings().find(".button-input").html($(this).find("a").html());
-     
-     
-    });
- $(".nav-button").on("click",function(){
+$(".dropdown>ul li").on("click", function() {
+  $(this).parent().siblings().find(".button-input").html($(this).find("a").html());
 
-        var aps = $(".nav-panel");
-       aps.animate({"right":" 0 ","opacity":"1"},300)
-      
 
-    });
-    $(".menu-close").on("click",function(){
-        var aps = $(".nav-panel");
-       aps.animate({"right":"-1000","opacity":"1"},300)
-      
-    });
+});
+$(".nav-button").on("click", function() {
+
+  var aps = $(".nav-panel");
+  aps.animate({
+    "right": " 0 ",
+    "opacity": "1"
+  }, 300)
+
+
+});
+$(".menu-close").on("click", function() {
+  var aps = $(".nav-panel");
+  aps.animate({
+    "right": "-1000",
+    "opacity": "1"
+  }, 300)
+
+});
+  $(".booking-therapist").on("click", function() {
+  console.log(1);
+
+});
